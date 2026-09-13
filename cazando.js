032ba90243cf60795608768f5a13a1d1028251e2
theGebus <diegoaor24@gmail.com>
2026-09-13 14:21:40 -0500

    Declare  las nuevas varibles y constantes del gato y su comida

--- cazando.js
+++ cazando.js
@@ -1,12 +1,35 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
-function llamarFunciones(){
+//variable de posicion de los objetos
+
+let gatoX=canvas.width/2;
+let gatoY=425;
+let comidaX=460;
+let comidaY=460;
+
+//Poner las constantes de los objetos
+
+const alto_Gato=75;
+const ancho_Gato=50;
+const alto_Comida=40;
+const ancho_Comida=40;
+
+
+function iniciarJuego(){
     graficarGato();
+    graficarComida();
 }
+//DIbujo de gato 
 
 function graficarGato(){
-
 ctx.fillStyle="black";
-ctx.fillRect(canvas.width/2, 50, 75, 50);
+ctx.fillRect(gatoX,gatoY, ancho_Gato, alto_Gato);
+}
+
+//dibujo de comida del Gato
+
+function graficarComida(){
+    ctx.fillStyle="orange";
+    ctx.fillRect(comidaX, comidaY, ancho_Comida, alto_Comida); 
 }
