12c43f6a320070f229abc53a012bdc2b7aeb0112
gebus <diegoaor24@gmail.com>
2026-09-13 19:59:27 -0500

    Corregir posición inicial del gato en el canvas

--- cazando.js
+++ cazando.js
@@ -1,19 +1,19 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
-//variable de posicion de los objetos
-
-let gatoX=canvas.width/2;
-let gatoY=425;
-let comidaX=460;
-let comidaY=460;
-
 //Poner las constantes de los objetos
 
 const alto_Gato=75;
 const ancho_Gato=50;
 const alto_Comida=40;
 const ancho_Comida=40;
+
+//variable de posicion de los objetos
+
+let gatoX=(canvas.width-ancho_Gato)/2;
+let gatoY=425;
+let comidaX=460;
+let comidaY=460;
 
 
 function iniciarJuego(){
